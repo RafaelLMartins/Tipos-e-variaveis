b0a7c99134da103adbef5a74621a36d3e8d940ea
RafaelLMartins <rafaelluism@hotmail.com>
2023-03-13 16:24:44 -0300

    Tipos de Variáveis 3 e 4.

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,21 @@
+import java.sql.SQLOutput;
+
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double salarioMninimo = 5000;// TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println("Variavel: "+ numero);
+
+        final int VALOR_NUNCA_PODE_MUDAR=5;
+
+        System.out.println("Constante: "+ VALOR_NUNCA_PODE_MUDAR);
     }
 }
